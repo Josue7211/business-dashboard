1fa7b0f394f68c07309f46ab52b3aa2bfd7bb1a6
Josue7211 <bobbyparzero@gmail.com>
2023-04-08 15:49:54 -0400

    rafce

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
               <Route path='/financial' element="Financial" />
               <Route path='/color-mapping' element="ColorMapping" />
               <Route path='/pyramid' element="Pyramid" />
-
+              <Route path='/stacked' element="Stacked" />
             </Routes>
           </div>
 
