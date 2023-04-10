ca1eefc248ddbe98405ff341304ba751fb30efca
Josue7211 <bobbyparzero@gmail.com>
2023-04-09 22:59:47 -0400

    Login, working but fix private routes

--- src/App.js
+++ src/App.js
@@ -1,84 +1,90 @@
-import React, { useEffect }from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups'
-
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
-import { Ecommerce, Invoices, Calendar, Team, Stacked, Pyramid, Clients, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line  } from './pages'
-
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useStateContext } from './contexts/ContextProvider'
-
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
+import { Ecommerce, Invoices, Calendar, Team, Stacked, Pyramid, Clients, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line, Login  } from './pages'
 import './App.css'
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode, user } = useStateContext();
   
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
-        <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
-            <TooltipComponent content='Settings' position='Top'>
-              <button 
-                type='button'
-                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                style={{ background: currentColor, borderRadius: '50%'}}
-                onClick={() => setThemeSettings(true)}
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-            </div>
-            {activeMenu ? (
-              <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-                <Sidebar />
+        {user ? (
+          <>
+            <div className='flex relative dark:bg-main-dark-bg'>
+              <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+                <TooltipComponent content='Settings' position='Top'>
+                  <button 
+                    type='button'
+                    className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                    style={{ background: currentColor, borderRadius: '50%'}}
+                    onClick={() => setThemeSettings(true)}
+                  >
+                    <FiSettings />
+                  </button>
+                </TooltipComponent>
               </div>
-            ) : (
-              <div className='w-0 dark:bg-secondary-dark-bg'>
-                <Sidebar />
+              {activeMenu ? (
+                <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                  <Sidebar />
+                </div>
+              ) : (
+                <div className='w-0 dark:bg-secondary-dark-bg'>
+                  <Sidebar />
+                </div>
+              )}
+              <div className={
+                `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2 '}`
+              }>
+                <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                  <Navbar />
+                </div>
+  
+              <div>
+                {themeSettings && <ThemeSettings />}
+                <Routes>
+                  {/* Dashboard */}
+                  <Route path='/' element={<Ecommerce />} />
+                  <Route path='/ecommerce' element={<Ecommerce />} />
+
+                  {/* Pages */}
+                  <Route path='/invoices' element={<Invoices />} />
+                  <Route path='/team' element={<Team />} />
+                  <Route path='/clients' element={<Clients />} />
+
+                  {/* Apps */}
+                  <Route path='/kanban' element={<Kanban />} />
+                  <Route path='/editor' element={<Editor />} />
+                  <Route path='/calendar' element={<Calendar />} />
+                  <Route path='/color-picker' element={<ColorPicker />} />
+
+                  {/* Charts */}
+                  <Route path='/line' element={<Line />} />
+                  <Route path='/area' element={<Area />} />
+                  <Route path='/bar' element={<Bar />} />
+                  <Route path='/pie' element={<Pie />} />
+                  <Route path='/financial' element={<Financial />} />
+                  <Route path='/color-mapping' element={<ColorMapping />} />
+                  <Route path='/pyramid' element={<Pyramid />} />
+                  <Route path='/stacked' element={<Stacked />} />
+                </Routes>
               </div>
-            )}
-            <div className={
-              `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2 '}`
-            }>
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-              <Navbar />
-            </div>
-
-            <div>
-              {themeSettings && <ThemeSettings />}
-              <Routes>
-                {/* Dashboard */}
-                <Route path='/' element={<Ecommerce />} />
-                <Route path='/ecommerce' element={<Ecommerce />} />
-
-                {/* Pages */}
-                <Route path='/invoices' element={<Invoices />} />
-                <Route path='/team' element={<Team />} />
-                <Route path='/clients' element={<Clients />} />
-
-                {/* Apps */}
-                <Route path='/kanban' element={<Kanban />} />
-                <Route path='/editor' element={<Editor />} />
-                <Route path='/calendar' element={<Calendar />} />
-                <Route path='/color-picker' element={<ColorPicker />} />
-
-                {/* Charts */}
-                <Route path='/line' element={<Line />} />
-                <Route path='/area' element={<Area />} />
-                <Route path='/bar' element={<Bar />} />
-                <Route path='/pie' element={<Pie />} />
-                <Route path='/financial' element={<Financial />} />
-                <Route path='/color-mapping' element={<ColorMapping />} />
-                <Route path='/pyramid' element={<Pyramid />} />
-                <Route path='/stacked' element={<Stacked />} />
-              </Routes>
             </div>
           </div>
-        </div>
+          </>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path='*' element={<Navigate to='/login' />} />
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,6 @@
 import React, { createContext, useContext, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../Firebase';
 
 const StateContext = createContext()
 
@@ -16,6 +18,9 @@ export const ContextProvider = ({ children }) => {
     const [currentColor, setCurrentColor] = useState('#03C9D7')
     const [currentMode, setCurrentMode] = useState('Light')
     const [themeSettings, setThemeSettings] = useState(false)
+    const [user, loading, error] = useAuthState(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     
     const setMode = (e) =>  {
         setCurrentMode(e.target.value)
@@ -53,6 +58,13 @@ export const ContextProvider = ({ children }) => {
                 setThemeSettings,
                 setMode,
                 setColor,
+                user,
+                loading,
+                error,
+                email,
+                setEmail,
+                password,
+                setPassword,
             }}
         >
             {children}
